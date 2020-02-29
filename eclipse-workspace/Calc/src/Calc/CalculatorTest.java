07daf24826b1090cb43e9aa6a615a9d65bcb834a
Salaheddin Badawi <salahbadawi12@gmail.com>
2020-02-29 22:13:32 +0200

    t

---
+++ eclipse-workspace/Calc/src/Calc/CalculatorTest.java
@@ -0,0 +1,42 @@
+package Calc;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAddTwoPositive() {
+		int first=1;
+		int second=2;
+		Calculator ca = new Calculator();
+		int result = ca.add(first,second);
+		assertTrue(result>0);
+			}
+	@Test
+	public void testAddTwoNegative() {
+		int first=-1;
+		int second=-2;
+		Calculator ca = new Calculator();
+		int result = ca.add(first,second);
+		assertTrue(result<0);
+	}
+	@Test
+	public void testAddOnePositiveisGreaterOneNegative() {
+		int first=-1;
+		int second=2;
+		Calculator ca = new Calculator();
+		int result = ca.add(first,second);
+		assertTrue(result>0);
+	}
+	@Test
+	public void testAddOnePositiveOneNegativeisGreater() {
+		int first=1;
+		int second=-2;
+		Calculator ca = new Calculator();
+		int result = ca.add(first,second);
+		assertTrue(result<0);
+	}
+	
+
+}
